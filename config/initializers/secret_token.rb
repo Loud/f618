ca19fc3efec9ad0f8d9f325d63412da16cb2295a
Marcus <marcus@fabrik618.se>
2011-05-19 16:42:25 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+F618::Application.config.secret_token = '7f32ffdf3b3ffe846524be7ca0d6277e37a547eabd727a8c0e105f0e1dfd5849243c993485fbc02803d3ab371f06d7a45c621f88f669be0b956aa7334eb89057'
