95203883bb7d41895b53f81b5e56cf5e979bb4ed
Marcus <marcus@fabrik618.se>
2011-05-22 19:39:03 +0200

    initial commit

---
+++ public/javascripts/calls.js
@@ -0,0 +1,14 @@
+					Cufon.replace('span.smallBold', { fontFamily: 'Gill' });
+
+			Cufon.replace('li span.smallBold', { fontFamily: 'BebasNeue' });
+			Cufon.replace('a.anchorlink', { fontFamily: 'BebasNeue' });
+			Cufon.replace('span.buttonLabel', {fontFamily: 'BebasNeue' });
+			Cufon.replace('h2.sumheader', { fontFamily: 'BebasNeue' });
+			Cufon.replace('span.testimonial', { fontFamily: 'Gill' });		
+			Cufon.replace('h2.servicesheader', { fontFamily: 'Gill' });
+			Cufon.replace('h3.servicesheadersmall', { fontFamily: 'BebasNeue' });			
+			Cufon.replace('h2.portfolioheader', { fontFamily: 'BebasNeue' });
+			Cufon.replace('h3.portfoliosmall', { fontFamily: 'BebasNeue'});			
+			Cufon.replace('h2.contactheader', { fontFamily: 'BebasNeue' });
+			Cufon.replace('h2.contactinfo', { fontFamily: 'BebasNeue' });			
+			Cufon.replace('div#contact span.smallBold', {fontFamily: 'Gill' });
